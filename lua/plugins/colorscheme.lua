1fdbaa50ac15395420085e24d46251f1b5ccd7c8
Jan Haering <jan.haering@medevio.cz>
2024-04-09 14:06:14 +0200

    Lets do it! (my initial setup)

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin",
+      colorscheme = "tokyonight",
+      -- colorscheme = "gruvbox",
+    },
+  },
+}
